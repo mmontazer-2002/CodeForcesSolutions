c7ed4210979901201fece18a707f52578557b7e7
Mahdie Montazeri <mmontazer1831@gmail.com>
2022-02-24 01:29:29 +0330

    add new condition to watermelon solution

--- A. Watermelone/src/Main.java
+++ A. Watermelone/src/Main.java
@@ -7,12 +7,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int weight = scanner.nextInt();
-        if (weight % 2 == 0) {
-            if (weight == 2) {
-                System.out.println("NO");
-            } else {
+        if (weight % 2 == 0 && weight != 2) {
                 System.out.println("YES");
-            }
+
         } else {
             System.out.println("NO");
         }
