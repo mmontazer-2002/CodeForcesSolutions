34c28b77ab269467ede8aced450d355de4f01eab
Mahdie Montazeri <mmontazer1831@gmail.com>
2022-02-23 22:11:44 +0330

    The answer to A.Watermelon question in codeforces.com

---
+++ A.Watermelone/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+/**
+ * Created by Mahdie Montazeralzohoor on 2/18/2022 7:24 PM
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int weight = scanner.nextInt();
+        if (weight % 2 == 0) {
+            if (weight == 2) {
+                System.out.println("NO");
+            } else {
+                System.out.println("YES");
+            }
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
